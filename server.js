fada36639f1d553475ca3ba557ff0475159ae0e0
Nelixao <ncelisalonso@gmail.com>
2025-02-11 22:22:47 -0600

    esta el modelo de la libreia tiene el buscador

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-// Ruta principal
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta principal, que ahora cargará el index.html
 app.get('/', (req, res) => {
-  res.send('Bienvenido al servidor');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Otros controladores que necesites agregar
